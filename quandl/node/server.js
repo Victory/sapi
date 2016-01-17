31b330bffa61ba3c2aea8d37be9a5a4a98d50a53
Victory <git@dfhu.org>
2016-01-17 14:15:26 -0500

    getting some info from CBOE

--- quandl/node/server.js
+++ quandl/node/server.js
@@ -3,5 +3,5 @@ var stuff = require('./stuff');
 var app = express();
 
 app.get('/', stuff.home); 
-
+app.get('/c', stuff.price);
 app.listen(3000);
--- quandl/node/stuff.js
+++ quandl/node/stuff.js
@@ -1,7 +1,36 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var Quandl = require("quandl");
+
+
+var api = {
+  get key() {
+    var apiKeyPath = path.join(__dirname, '../quandl.apikey');
+    var key = fs.readFileSync(apiKeyPath, {encoding: 'utf-8'});
+    return key.trim();
+  }
+};
+
+var quandl = new Quandl();
+quandl.configure({
+    auth_token: api.key 
+});
+
 var stuff = {
     home: function (req, res) {
         res.send('Quandl home');
-    }
+    },
+    price: function (req, res) {
+        quandl.dataset(
+            { source: "CBOE/VXEEM", format: 'json'}, 
+            function(err, response){
+                if(err)
+                    throw err;
+
+            res.send(response); 
+        });
+    },
 };
 
 
