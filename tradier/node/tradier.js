f64fe56a95075a6297c4612dae9545f1886c39cb
Victory <git@dfhu.org>
2016-01-21 18:06:01 -0500

    refactor getData

--- tradier/node/tradier.js
+++ tradier/node/tradier.js
@@ -3,29 +3,27 @@ var fs = require('fs');
 var path = require('path');
 
 var api = {
-  get key() {
-    var apiKeyPath = path.join(__dirname, '../tradier.apikey');
-    var key = fs.readFileSync(apiKeyPath, {encoding: 'utf-8'});
-    return key.trim();
-  },
+    get key() {
+        var apiKeyPath = path.join(__dirname, '../tradier.apikey');
+        var key = fs.readFileSync(apiKeyPath, {encoding: 'utf-8'});
+        return key.trim();
+    },
 
-  get options() {
-    return {
-        host: "sandbox.tradier.com",
-        path: "/v1/markets/quotes?symbols=",
-        method: "GET",
-        headers: {
-            "Accept": "application/json",
-            "Authorization": "Bearer " + api.key
-        }
-    };
-  }
-};
+    get options() {
+        return {
+            host: "sandbox.tradier.com",
+            path: "/v1/markets",
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + api.key
+            }
+        };
+    },
 
-var tradier = {
-    quotes: function (smb, fn) {
+    getData: function (path, fn) {
         var options = api.options;
-        options.path += smb;
+        options.path += path; 
         var body = "";
         var request = https.request(options, function (response) {
             response.on('data', function (data) {
@@ -40,7 +38,13 @@ var tradier = {
         request.on('error', function(e) {
             console.error(e);
         });
+    },   
+};
 
+var tradier = {
+    quotes: function (smb, fn) {
+        var path = "/quotes?symbols=" + smb;
+        api.getData(path, fn);
         return null;
     }
 };
