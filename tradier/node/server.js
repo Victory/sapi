fdeb8ce8ecb5492d40d1e790aa76b732d3f4bbe1
Victory <git@dfhu.org>
2016-01-28 03:32:58 -0500

    use absolute path to sandbox

--- tradier/node/server.js
+++ tradier/node/server.js
@@ -7,7 +7,8 @@ nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
-app.use(express.static('static'));
+app.use(express.static(__dirname + 'static'));
+
 app.get('/', site.home);
 app.get('/q/:smb', site.quote);
 app.get('/c/:smb/:expiry', site.chains);
