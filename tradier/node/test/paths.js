c3e4a61b598eb9af51f54fecb84473172b1edf53
Victory <git@dfhu.org>
2016-02-03 17:45:02 -0500

    create mockable transport layer

--- tradier/node/test/paths.js
+++ tradier/node/test/paths.js
@@ -1,9 +1,17 @@
 const assert = require('assert');
 const tradier = require('../tradier');
+var mockTransport = {
+    request: function (options, success) {
+        success();
+    },
+};
+
+
 describe('tradier', function () {
     var quotesUriForSpy = "https://sandbox.tradier.com/v1/markets/quotes?symbols=spy";
     describe('#quotes()', function () {
-        it('should call ' + quotesUriForSpy + ' when passed spy', function (done) {
+        it('should call ' + quotesUriForSpy + ' when passed "spy"', function (done) {
+            tradier.setTransport(mockTransport);
             tradier.quotes('spy', function () {
                 assert.equal(tradier.getLastUriCalled(), quotesUriForSpy);
                 done();
--- tradier/node/tradier.js
+++ tradier/node/tradier.js
@@ -2,9 +2,29 @@ const https = require('https');
 var fs = require('fs');
 var path = require('path');
 
+var httpTransport = {
+    request: function (options, success) {
+        console.log('real request');
+        var body = "";
+        var request = https.request(options, function (response) {
+            response.on('data', function (data) {
+                body += data;
+            });
+            response.on('end', function () {
+                success(body);
+            });
+        });
+        request.end();
+
+        request.on('error', function(e) {
+            console.error(e);
+        });
+    },
+};
+
 var api = {
     lastUriCalled: null,
-
+    transport: null,
     get key() {
         var apiKeyPath = path.join(__dirname, '../tradier.apikey');
         var key = fs.readFileSync(apiKeyPath, {encoding: 'utf-8'});
@@ -22,33 +42,25 @@ var api = {
             }
         };
     },
+    setTransport: function (transport) {
+        this.transport = transport;
+    },
 
     getData: function (path, fn) {
         var options = api.options;
         options.path += path; 
         this.lastUriCalled = "https://" + options.host + options.path;
-        var body = "";
-        var request = https.request(options, function (response) {
-            response.on('data', function (data) {
-                body += data;
-            });
-            response.on('end', function () {
-                fn(body);
-            });
-        });
-        request.end();
-
-        request.on('error', function(e) {
-            console.error(e);
-        });
-    }, 
+        this.transport.request(options, fn);
+     }, 
 };
 
 var tradier = {
     getLastUriCalled: function () { 
         return api.lastUriCalled; 
     },
-
+    setTransport: function (transport) {
+        api.setTransport(transport);
+    },
     // ===== pricing/market data
     quotes: function (smb, fn) {
         var path = "/markets/quotes?symbols=" + smb;
@@ -71,5 +83,7 @@ var tradier = {
         api.getData(path, fn);
     }
 };
+
+tradier.setTransport(httpTransport);
 
 module.exports = tradier;
