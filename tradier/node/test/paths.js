435ce6b5c064607056efb8e65c4b26610517c834
Victory <git@dfhu.org>
2016-02-04 17:55:34 -0500

    test for chains

--- tradier/node/test/paths.js
+++ tradier/node/test/paths.js
@@ -14,6 +14,8 @@ describe('tradier', function () {
     var quotesUriForSpy = "https://sandbox.tradier.com/v1/markets/quotes?symbols=spy";
     var quotesUriForMsft = "https://sandbox.tradier.com/v1/markets/quotes?symbols=msft";
     var quotesUriForF = "https://sandbox.tradier.com/v1/markets/quotes?symbols=f";
+
+    var chainsUri = "https://sandbox.tradier.com/v1/markets/options/chains?symbol=spy&expiration=2019-01-01";
     describe('#quotes()', function () {
         it('should call ' + quotesUriForSpy + ' when passed "spy"', function (done) {
             tradier.setTransport(mockTransport);
@@ -31,6 +33,15 @@ describe('tradier', function () {
             assert.equal(spyInfo.uri, quotesUriForSpy);
             assert.equal(msftInfo.uri, quotesUriForMsft);
             setTimeout(done, 200);
+        });
+    });
+
+    describe('#chains()', function () {
+        it('should call ' + chainsUri +  ' when passed "spy" expiry 2020-01-01', function (done) {
+            tradier.setTransport(mockTransport);
+            var chains = tradier.chains('spy', '2019-01-01', function () {}); 
+            assert.equal(chains.uri, chainsUri);
+            done();
         });
     });
 });
