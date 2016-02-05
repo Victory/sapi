9c21a0875d33c036371d20f1014846e8c6315b71
Victory <git@dfhu.org>
2016-02-05 16:36:59 -0500

    put in test for timesales calledUri

--- tradier/node/test/paths.js
+++ tradier/node/test/paths.js
@@ -14,8 +14,9 @@ describe('tradier', function () {
     var quotesUriForSpy = "https://sandbox.tradier.com/v1/markets/quotes?symbols=spy";
     var quotesUriForMsft = "https://sandbox.tradier.com/v1/markets/quotes?symbols=msft";
     var quotesUriForF = "https://sandbox.tradier.com/v1/markets/quotes?symbols=f";
-
     var chainsUri = "https://sandbox.tradier.com/v1/markets/options/chains?symbol=spy&expiration=2019-01-01";
+    var timeSalesUri = "https://sandbox.tradier.com/v1/markets/timesales?symbol=spy";
+
     describe('#quotes()', function () {
         it('should call ' + quotesUriForSpy + ' when passed "spy"', function (done) {
             tradier.setTransport(mockTransport);
@@ -41,6 +42,15 @@ describe('tradier', function () {
             tradier.setTransport(mockTransport);
             var chains = tradier.chains('spy', '2019-01-01', function () {}); 
             assert.equal(chains.uri, chainsUri);
+            done();
+        });
+    });
+
+    describe('#timesales()', function () {
+        it('should call  ' + timeSalesUri + ' when passed "spy"', function (done) {
+            tradier.setTransport(mockTransport);
+            var timesales = tradier.timesales('spy', function () {});
+            assert.equal(timesales.uri, timeSalesUri);
             done();
         });
     });
